40aabfc75abad4fcd5a16c5cf871dc545537478f
mihayash <mihayash@microsoft.com>
2020-04-13 14:01:41 +0900

    Commit1
    Commit1 Description

---
+++ RecvIntent/RecvIntent/RecvIntent.Android/Activity1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace RecvIntent.Droid
+{
+    [Activity(Label = "Activity1", Name = "com.companyname.recvintent.activity1", MainLauncher = true)]
+    public class Activity1 : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            System.Diagnostics.Debug.Print("Activity1.OnCreate");
+
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            System.Diagnostics.Debug.Print("Activity1.OnNewIntent");
+
+            if (intent.Extras != null)
+            {
+
+
+            }
+
+            base.OnNewIntent(intent);
+        }
+
+    }
+}
---
+++ RecvIntent/RecvIntent/RecvIntent/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RecvIntent
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
